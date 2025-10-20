d876d5be468398ef5b160e748ac68f49448b11a1
msruhan <masruhan.khotibb@gmail.com>
2025-10-20 22:08:38 +0700

    Add comment section with reply functionality and enhance mobile navbar layout

--- src/pages/explore/explore-marketplace.vue
+++ src/pages/explore/explore-marketplace.vue
@@ -3,16 +3,17 @@
         <navbar :navLight="'nav-light'" :logoLight=true  />
         
         <!-- Start Hero -->
-        <section class="relative table w-full py-36 bg-[url('../../assets/images/bg/bg1.jpg')] bg-bottom bg-no-repeat">
-            <div class="absolute inset-0 bg-gradient-to-b from-transparent to-slate-900"></div>
+        <!-- Start Hero (Simple for Mobile) -->
+        <section class="relative w-full py-20 md:py-36 bg-[url('../../assets/images/bg/bg1.jpg')] md:bg-bottom md:bg-no-repeat bg-none">
+            <div class="absolute inset-0 bg-gradient-to-b from-transparent to-slate-900 md:block hidden"></div>
             <div class="container">
-                <div class="grid grid-cols-1 pb-8 text-center mt-10">
-                    <h3 class="md:text-3xl text-2xl md:leading-snug tracking-wide leading-snug font-medium text-white">Pusat Produk / Barang</h3>
-                </div><!--end grid-->
-            </div><!--end container-->
-        </section><!--end section-->
+                <div class="grid grid-cols-1 pb-4 text-center mt-6 md:pb-8 md:mt-10">
+                    <h3 class="text-xl md:text-3xl md:leading-snug tracking-wide leading-snug font-medium text-black">Pusat Produk / Barang</h3>
+                </div>
+            </div>
+        </section>
 
-        <div class="relative">
+        <div class="relative md:block hidden">
             <div class="shape absolute start-0 end-0 sm:-bottom-px -bottom-[2px] overflow-hidden z-1 text-white dark:text-slate-900">
                 <svg class="w-full h-auto" viewBox="0 0 2880 48" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0 48H1437.5H2880V0H2160C1442.5 52 720 0 720 0H0V48Z" fill="currentColor"></path>
